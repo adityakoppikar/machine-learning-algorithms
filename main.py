982fac743107485604dd41401ed9fe6407f82f4a
adityakoppikar <107309895+adityakoppikar@users.noreply.github.com>
2022-08-30 22:09:36 +0530

    Add files via upload
    t

---
+++ main.py
@@ -0,0 +1,20 @@
+class MySimpleLinearRegressor:
+
+    def __init__(self):
+        self.m = None
+        self.b = None
+
+    def fit(self, X_train, y_train):
+
+        num = 0
+        den = 0
+        for i in range(X_train.shape[0]):
+
+            num = num + ((X_train[i] - X_train.mean()) * (y_train[i] - y_train.mean()))
+            den = den + ((X_train[i] - X_train.mean()) * (X_train[i] - X_train.mean()))
+
+        self.m = num / den
+        self.b = y_train.mean() - (self.m * X_train.mean())
+
+    def predict(self, X_test):
+        return (self.m * X_test) + self.b
